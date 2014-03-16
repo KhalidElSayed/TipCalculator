a272b19cf2da83a08c5016f04a20cb8e8b58e0a3
Inez Korczyński <inez@wikia-inc.com>
2014-03-15 19:50:22 -0700

    First, completely working version

--- gen/com/example/tipcalculator/R.java
+++ gen/com/example/tipcalculator/R.java
@@ -24,8 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int editText1=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int bill_amount=0x7f080000;
+        public static final int tip_amount=0x7f080004;
+        public static final int toggle_10=0x7f080003;
+        public static final int toggle_15=0x7f080001;
+        public static final int toggle_20=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,7 +40,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int tip_is=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/example/tipcalculator/MainActivity.java
+++ src/com/example/tipcalculator/MainActivity.java
@@ -1,15 +1,59 @@
 package com.example.tipcalculator;
 
-import android.os.Bundle;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
+	EditText bill_amount;
+	TextView tip_amount;
+	ToggleButton toggle_10;
+	ToggleButton toggle_15;
+	ToggleButton toggle_20;
+	ToggleButton active_toggle;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		bill_amount = (EditText) findViewById(R.id.bill_amount);	 
+		tip_amount = (TextView) findViewById(R.id.tip_amount);	 
+		toggle_10 = (ToggleButton) findViewById(R.id.toggle_10);
+		toggle_15 = (ToggleButton) findViewById(R.id.toggle_15);
+		toggle_20 = (ToggleButton) findViewById(R.id.toggle_20);
+
+		toggle_10.setOnCheckedChangeListener(changeChecker);
+		toggle_15.setOnCheckedChangeListener(changeChecker);
+		toggle_20.setOnCheckedChangeListener(changeChecker);
+		
+		bill_amount.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void afterTextChanged(Editable s) {
+				calculateTip();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+			}
+		});
+		
+		setTipAmount(0);
 	}
 
 	@Override
@@ -18,5 +62,61 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void calculateTip() {
+		if(active_toggle==null){
+			setTipAmount(0);
+			return;
+		}
+
+		float billAmount;
+		try {
+			billAmount = Float.valueOf(bill_amount.getText().toString());
+		} catch (Exception e) {
+			setTipAmount(0);
+			return;
+		}
+		
+		float tipPercentage = 0;
+		if ( active_toggle == toggle_10 ) {
+			tipPercentage = 10;			
+		} else if ( active_toggle == toggle_15 ) {
+			tipPercentage = 15;			
+		} else if ( active_toggle == toggle_20 ) {
+			tipPercentage = 20;			
+		}
+		
+		setTipAmount(billAmount * tipPercentage / 100);
+	}
+	
+	private void setTipAmount(float amount) {
+		tip_amount.setText(
+				getString(
+						R.string.tip_is,
+						NumberFormat.getCurrencyInstance().format(amount).replaceAll("\\.00", "")
+				)
+		);
+	}
+	
+	public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+	
+	OnCheckedChangeListener changeChecker = new OnCheckedChangeListener() {
+		@Override
+	    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+			if ( isChecked ) {
+				if ( active_toggle != null ) {
+					active_toggle.setChecked(false);
+				}
+				active_toggle = (ToggleButton) buttonView;
+			} else {
+				active_toggle = null;
+			}
+			calculateTip();
+		}
+	};
 
 }
